9b7a3eab49dc873e83105a1a1c36b7f865486c96
thefreebear <adityasuratgarh5@gmail>
2025-03-07 11:32:24 +0530

    automating CI pipeline.

---
+++ test_app.py
@@ -0,0 +1,15 @@
+from app import app
+import json
+import pytest
+
+
+@pytest.fixture
+def client():
+    return app.test_client()
+
+
+def test_hello_world():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == '<p>Hello, World!</p>'
+
