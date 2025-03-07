1058e558d94c2d35392caab0f2b5193ee68bf845
thefreebear <adityasuratgarh5@gmail>
2025-03-07 11:40:46 +0530

    making the versions of dependencies correct

--- test_app.py
+++ test_app.py
@@ -8,7 +8,7 @@ def client():
     return app.test_client()
 
 
-def test_hello_world():
+def test_hello_world(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.data.decode('utf-8') == '<p>Hello, World!</p>'
